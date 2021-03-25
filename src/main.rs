6402c55c2625c30a4b7a7ff72ff968c2ff366607
curtain <1410768946@qq.com>
2021-03-26 00:41:37 +0800

    add panic_handler test_frameworks version with conditional compile feature and redirect output to serial

--- src/main.rs
+++ src/main.rs
@@ -44,9 +44,19 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 }
 
 // define self panic handler
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
