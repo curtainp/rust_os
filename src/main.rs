f4d463b6445ab03450cb7e83b71dfe7d2753637f
curtain <1410768946@qq.com>
2021-03-26 00:19:13 +0800

    add qemu exit funtion

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
@@ -24,6 +25,21 @@ fn trivial_assertion() {
     print!("trivial_assertion...");
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 // define self panic handler
