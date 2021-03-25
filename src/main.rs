d3aecedee2f335e3a4321b00863ae43666d97deb
curtain <1410768946@qq.com>
2021-03-25 23:56:51 +0800

    add ourself test frameworks

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@
 #![no_std]
 // disable runtime entry
 #![no_main]
+// build ourself test frameworks
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("running {} test case", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 // define self panic handler
 #[panic_handler]
@@ -17,6 +28,5 @@ fn panic(_info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello Rust OS{}", "!");
-    panic!("Some panic message");
     loop {}
 }
