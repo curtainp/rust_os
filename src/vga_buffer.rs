d1b2897b4fb80e1869cbf98c1fd6755388795703
curtain <1410768946@qq.com>
2021-03-26 15:55:13 +0800

    add test case for vga_buffer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -4,6 +4,8 @@ use volatile::Volatile;
 use core::fmt;
 use lazy_static::lazy_static;
 use spin::Mutex;
+// use when test case
+use crate::{serial_print, serial_println};
 
 lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
@@ -147,4 +149,35 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+// test case here
+#[test_case]
+fn test_println_simple() {
+    serial_print!("test_println....");
+    println!("test_simple_println....");
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_many_line() {
+    serial_print!("test_println_many_line....");
+    for _ in 0..200 {
+        println!("test_println_many_line...");
+    }
+    serial_println!("[ok]");
+}
+// assert to ensure character output to screen
+#[test_case]
+fn test_println_output() {
+    serial_print!("test_println_output...");
+
+    let s = "some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+
+    serial_println!("[ok]");
 }
