5e6f7140255de2b53fdba897f4f9649a7a01c115
curtain <1410768946@qq.com>
2021-03-26 16:48:59 +0800

    first integration test case

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use blog_os::{println, serial_print, serial_println};
 use core::panic::PanicInfo;
 
 #[no_mangle]
@@ -14,9 +15,13 @@ pub extern "C" fn _start() -> ! {
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    blog_os::test_panic_handler(info)
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
+// this test case neccessary, becase it don't call any initial func
+#[test_case]
+fn test_println() {
+    serial_print!("test println ....");
+    println!("some simple test for println ....");
+    serial_println!("[ok]");
 }
