170c7cff6c492fc5915df2fb8558e3b94835f232
curtain <1410768946@qq.com>
2021-03-29 15:36:11 +0800

    define a static IDT object for cpu, otherwise, IDT will destory when fn over.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,9 +1,13 @@
 use crate::println;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
+static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+
 pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    unsafe {
+        IDT.breakpoint.set_handler_fn(breakpoint_handler);
+        IDT.load();
+    }
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
