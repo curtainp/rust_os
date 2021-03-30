39e80084da3c391989d1315de5a79007d73d4aab
curtain <1410768946@qq.com>
2021-03-30 18:56:50 +0800

    create tss in gdt mod

---
+++ src/gdt.rs
@@ -0,0 +1,25 @@
+use lazy_static::lazy_static;
+use x86_64::structures::tss::TaskStateSegment;
+use x86_64::VirtAddr;
+
+// this value can change between 0-6
+pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
+pub const PAGE_SIZE: usize = 4096;
+pub const PAGE_CNT: usize = 5;
+
+lazy_static! {
+    static ref TSS: TaskStateSegment = {
+      let mut tss = TaskStateSegment::new();
+      tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
+        const STACK_SIZE: usize = PAGE_CNT * PAGE_SIZE;
+        // TODO, this way is ugly
+        static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+
+        let stack_start = VirtAddr::from_ptr(unsafe{&STACK});
+        let stack_end = stack_start + STACK_SIZE;
+        //return top address, cause of stack from top to bottom
+        stack_end
+      };
+      tss
+    };
+}
