4d4b7c95d882f579acc76642023ea4915f9fb256
curtain <1410768946@qq.com>
2021-04-03 14:38:50 +0800

    fixing deadlock when we use print! ouput something and then trigger a interupt which handler also use print! to get the print lock

--- src/serial.rs
+++ src/serial.rs
@@ -14,10 +14,14 @@ lazy_static! {
 #[doc(hidden)]
 pub fn _print(args: ::core::fmt::Arguments) {
     use core::fmt::Write;
-    SERIAL
-        .lock()
-        .write_fmt(args)
-        .expect("print to serial failed");
+    use x86_64::instructions::interrupts;
+
+    interrupts::without_interrupts(|| {
+        SERIAL
+            .lock()
+            .write_fmt(args)
+            .expect("print to serial failed");
+    });
 }
 
 #[macro_export]
