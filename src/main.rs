5bac1e4bb576e878424c3655bda8dc06201f9326
curtain <1410768946@qq.com>
2021-06-29 10:40:44 +0800

    extract lang_items from main mod

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,7 @@
 // 禁用默认的入口点
 #![no_main]
 
-// 禁用标准库之后(link)，需要自己实现
-// 1. panic_handler
-// 2. exception_handler 语言项
-// 3. start 语言项
-
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    // TODO 程序panic时,进入死循环
-    loop {}
-}
+mod lang_items;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
