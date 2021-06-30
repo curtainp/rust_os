28fdc4fe02dc1549727752799f08a48a0f44930a
curtain <keleidoscope@163.com>
2021-06-30 21:28:25 +0800

    impl shutdown operator with sbi call

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,17 @@
 // 禁用默认的入口点
 #![no_main]
 #![feature(llvm_asm)]
+#![feature(global_asm)]
+use core::fmt::{self, Write};
+use crate::sbi::sbi_call;
+struct Stdout;
+
+mod lang_items;
+mod sbi;
 
 const SYSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
+const SBI_SHUTDOWN: usize = 8;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -21,10 +30,48 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
 pub fn sys_exit(estate: i32) -> isize {
     syscall(SYSCALL_EXIT, [estate as usize, 0, 0])
 }
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+pub fn shutdown() -> ! {
+    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    panic!("It should shutdown!");
+}
 
-mod lang_items;
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
 
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+
+// #[no_mangle]
+// pub extern "C" fn _start() {
+//     println!("hello world");
+//     shutdown();
+//     //sys_exit(9);
+// }
+
+global_asm!(include_str!("entry.asm"));
 #[no_mangle]
-pub extern "C" fn _start() {
-    sys_exit(9);
+pub fn rust_main() -> ! {
+    shutdown();
 }
